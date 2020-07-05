320352886a75e6c116af7f53c38a70c094d8b8aa
pragithub173 <pradeepkumarl173@gmail.com>
2020-07-04 23:33:29 -0500

    add arrow

--- notes-edit.js
+++ notes-edit.js
@@ -1,54 +1,50 @@
-const titleElement = document.querySelector('#note-title')
-const bodyElement = document.querySelector('#note-body')
-const removeElement = document.querySelector('#remove-note')
-const dateElement = document.querySelector('#last-edited')
-const noteId = location.hash.substring(1)
-let notes = getSavedNotes()
-let note = notes.find(function (note) {
-    return note.id === noteId
-})
+const titleElement = document.querySelector("#note-title");
+const bodyElement = document.querySelector("#note-body");
+const removeElement = document.querySelector("#remove-note");
+const dateElement = document.querySelector("#last-edited");
+const noteId = location.hash.substring(1);
+let notes = getSavedNotes();
+let note = notes.find((note) => note.id === noteId);
 
 if (note === undefined) {
-    location.assign('/index.html')
+  location.assign("/index.html");
 }
 
-titleElement.value = note.title
-bodyElement.value = note.body
-dateElement.textContent = generateLastEdited(note.updatedAt)
+titleElement.value = note.title;
+bodyElement.value = note.body;
+dateElement.textContent = generateLastEdited(note.updatedAt);
 
-titleElement.addEventListener('input', function (e) {
-    note.title = e.target.value
-    note.updatedAt = moment().valueOf()
-    dateElement.textContent = generateLastEdited(note.updatedAt)
-    saveNotes(notes)
-})
+titleElement.addEventListener("input", (e) => {
+  note.title = e.target.value;
+  note.updatedAt = moment().valueOf();
+  dateElement.textContent = generateLastEdited(note.updatedAt);
+  saveNotes(notes);
+});
 
-bodyElement.addEventListener('input', function (e) {
-    note.body = e.target.value
-    note.updatedAt = moment().valueOf()
-    dateElement.textContent = generateLastEdited(note.updatedAt)
-    saveNotes(notes)
-})
+bodyElement.addEventListener("input", (e) => {
+  note.body = e.target.value;
+  note.updatedAt = moment().valueOf();
+  dateElement.textContent = generateLastEdited(note.updatedAt);
+  saveNotes(notes);
+});
 
-removeElement.addEventListener('click', function (e) {
-    removeNote(note.id)
-    saveNotes(notes)
-    location.assign('/index.html')
-})
+removeElement.addEventListener("click", (e) => {
+  removeNote(note.id);
+  saveNotes(notes);
+  location.assign("/index.html");
+});
 
-window.addEventListener('storage', function (e) {
-    if (e.key === 'notes') {
-        notes = JSON.parse(e.newValue)
-        note = notes.find(function (note) {
-            return note.id === noteId
-        })
+window.addEventListener("storage", (e) => {
+  if (e.key === "notes") {
+    notes = JSON.parse(e.newValue);
+    note = notes.find((note) => note.id === noteId);
 
-        if (note === undefined) {
-            location.assign('/index.html')
-        }
-
-        titleElement.value = note.title
-        bodyElement.value = note.body
-        dateElement.textContent = generateLastEdited(note.updatedAt)
+    if (note === undefined) {
+      location.assign("/index.html");
     }
-})
+
+    titleElement.value = note.title;
+    bodyElement.value = note.body;
+    dateElement.textContent = generateLastEdited(note.updatedAt);
+  }
+});
