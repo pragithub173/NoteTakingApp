9f1b46489dead9c3bb0eb854c9e84e5fde17a05a
Pradeep Kumar Lokesh <pradeepkumarl173@gmail.com>
2021-06-09 14:05:39 -0500

    Update notes-app.js

--- notes-app.js
+++ notes-app.js
@@ -1,63 +1,4 @@
-// const para = document.querySelector("p");
-// console.log(para);
-// para.remove();
 
-// const paras = document.querySelectorAll("p");
-// console.log(paras);
-
-// paras.forEach(function (n) {
-//   n.textContent = "********";
-//   //   console.log(n.textContent); // used to show content with all the p tag
-//   //   n.remove();
-// });
-
-// //adding a new element
-// const newParagraph = document.createElement("p"); //creates a element by tag
-// newParagraph.textContent = "This is the newly added paragraph";
-// document.querySelector("body").appendChild(newParagraph);
-
-// const notes = [
-//   {
-//     title: "book1",
-//     description: "fruits",
-//   },
-//   {
-//     title: "book2",
-//     description: "veg",
-//   },
-//   {
-//     title: "not today",
-//     description: "nonveg",
-//   },
-// ];
-
-// const filters = {
-//   searchText: "",
-// };
-
-// const user = {
-//   name: "Pradeep",
-//   age: 25,
-// };
-// const userJSON = JSON.stringify(user);
-// // console.log(userJSON);
-
-// const UserDetails = localStorage.getItem("user");
-// const user = JSON.parse(UserDetails);
-// console.log(`${user.name} is ${user.age}`);
-
-// localStorage.setItem("user", userJSON); //setting upto local storge
-
-//localStorage.setItem("location", "Chicago");//set
-
-// localStorage.getItem("location");
-// console.log(localStorage.getItem("location"));//get
-
-//localStorage.setItem("location", "Illinois");//update
-
-//localStorage.removeItem("location");//removes key
-
-//localStorage.clear();//clears all the data
 "use.strict";
 let notes = getSavedNotes();
 
@@ -84,35 +25,13 @@ document.querySelector("#create-note").addEventListener("click", (e) => {
   location.assign(`/edit.html#${id}`);
 });
 
-// document.querySelector("#delete-note").addEventListener("click", function () {
-//   //console.log("Delete items");
-//   document.querySelectorAll(".note").forEach(function (notes) {
-//     notes.remove();
-//   });
-// });
 
-// document.querySelector("input").addEventListener("change", function (e) {
-//   e.target.value = "Value changed";
-// });
-
-// document.querySelector("input").addEventListener("change", function (e) {
-//   console.log(e.target.value);
-// });
 
 document.querySelector("#search-text").addEventListener("input", (e) => {
   filters.searchText = e.target.value;
   renderNotes(notes, filters);
 });
 
-// document.querySelector("#name-form").addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   console.log(e.target.elements.firstName.value);
-//   e.target.elements.firstName.value = "";
-// });
-
-// document.querySelector("#for-fun").addEventListener("change", function (e) {
-//   console.log(e.target.checked);
-// });
 document.querySelector("#filter-by").addEventListener("change", (e) => {
   filters.sortBy = e.target.value;
   renderNotes(notes, filters);
@@ -127,40 +46,3 @@ window.addEventListener("storage", (e) => {
   }
 });
 
-//dates
-
-// const now = moment(); //now has the date
-// //console.log(now);
-// console.log(now.toString());
-
-// //now.minute(1);
-
-// //console.log(now.toString()); //prints the minute set
-
-// //console.log(now.minute());  //prints the default minute
-
-// now.add(1, "year");
-
-// console.log(now.toString());
-
-// now.subtract(1, "week").subtract(20, "days");
-
-// console.log(now.toString());
-
-// console.log(now.format("MMM Do, YYYY"));
-
-// console.log(now.fromNow());
-
-// const nowTimeStamp = now.valueOf();
-
-// console.log(moment(nowTimeStamp).toString());
-
-// const bDate = moment();
-// console.log(bDate.toString()); //todays date
-
-// bDate.year(1994).month(9).day(36);
-
-// // bDate.subtract(25, "years").subtract(9, "months").add(30, "days");
-// console.log(bDate.format("MMM Do, YYYY"));
-
-// console.log(Date());
